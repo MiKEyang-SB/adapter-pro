09ba7a86a92f3bf9ef30f1d0839ff78c9ab04af2
MiKEyang-SB <910935770y@gmail.com>
2026-02-05 15:05:02 +0800

    stop tracking local run scripts

--- eval.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-setsid python experiments/robot/libero/run_libero_eval.py \
-    --task_suite_name libero_object \
-    --use_minivlm True \
-    --pretrained_checkpoint outputs/configs+libero_object_no_noops+b16+lr-0.0002+lora-r64+dropout-0.0--image_aug--90000_chkpt \
-    > eval.log 2>&1 &
-    
